ceca8f58b459757c2df3d5a84f3dd629a9e2dca7
Huntly Cameron <huntly.cameron@gmail.com>
2020-06-07 22:34:05 +0100

    Testing baby steps

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,3 +11,20 @@ test('renders without crashing', async () => {
     //Check for main h1 to ensure page has rendered
     expect(screen.queryAllByRole('heading')[0]).toHaveTextContent('Leccie Monitor');
 });
+
+describe('on first run', () => {
+    test('it should not have a previous readings table', () => {
+        render(<App dataStore={db} />);
+        expect(screen.queryByRole('table')).not.toBeInTheDocument();
+    });
+
+    test('instead, it should show enter first reading message', () => {
+        render(<App dataStore={db} />);
+        expect(screen.getByText('Enter your first reading to get started!')).toBeInTheDocument();
+    });
+
+    test('it should now show the alert bar', () => {
+        render(<App dataStore={db} />);
+        expect(screen.queryByText('Running low, go top up!!!')).not.toBeInTheDocument();
+    });
+})
