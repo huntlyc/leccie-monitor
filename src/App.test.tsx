3b36c5a5b33e4af3e474986ba8f1d5529087695a
Huntly Cameron <huntly.cameron@gmail.com>
2020-06-07 21:57:17 +0100

    Testing reading store

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { ReadingStorage } from './components/TestReadingStore';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const db: ReadingStorage = new ReadingStorage();
+
+test('renders without crashing', async () => {
+    render(<App dataStore={db} />);
+    //Check for main h1 to ensure page has rendered
+    expect(screen.queryAllByRole('heading')[0]).toHaveTextContent('Leccie Monitor');
 });
---
+++ src/components/TestReadingStore/index.tsx
@@ -0,0 +1,70 @@
+import Dexie from 'dexie';
+Dexie.dependencies.indexedDB = require('fake-indexeddb')
+Dexie.dependencies.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange')
+import { DBReading } from '../ReadingStore';
+
+export class ReadingStorage extends Dexie {
+    //Define our collection asserting (!:) that its never undefined
+    readings!: Dexie.Table<DBReading, number>;
+    constructor() {
+        super('TestReadingsDB');
+        //Our table(s) schema
+        this.version(1).stores({
+            readings: '++id, date, reading'
+        });
+        /**
+         * The following line is needed if your typescript is compiled using
+         * babel instead of tsc e.g. using 'react-create-app'
+         **/
+        this.readings = this.table("readings");
+    }
+    clearData() {
+        this.readings.clear();
+    }
+    seedData() {
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/05 23:32')).toISOString(),
+            reading: '50.00'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/12 08:22')).toISOString(),
+            reading: '31.63'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/16 12:22')).toISOString(),
+            reading: '23.20'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/18 19:01')).toISOString(),
+            reading: '61.03'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/20 14:01')).toISOString(),
+            reading: '44.44'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/21 14:01')).toISOString(),
+            reading: '41.03'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/25 17:41')).toISOString(),
+            reading: '37.28'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/26 10:41')).toISOString(),
+            reading: '35.10'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/26 19:41')).toISOString(),
+            reading: '34.19'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/27 10:21')).toISOString(),
+            reading: '29.23'
+        });
+        this.readings.add({
+            date: new Date(Date.parse('2019/10/27 11:20')).toISOString(),
+            reading: '29.04'
+        });
+    }
+}
