639cb1251b9d46064d36119819f30a62250eb3ea
Huntly Cameron <huntly.cameron@gmail.com>
2020-06-15 22:24:51 +0100

    Managed to get a unit test running after seeding the db

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from './App';
 import { ReadingStorage } from './components/TestReadingStore';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { render, fireEvent, waitFor, screen, wait, act } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 const db: ReadingStorage = new ReadingStorage();
@@ -27,4 +27,34 @@ describe('on first run', () => {
         render(<App dataStore={db} />);
         expect(screen.queryByText('Running low, go top up!!!')).not.toBeInTheDocument();
     });
-})
+});
+
+describe('on loading with previous saved values (seeded DB)', () => {
+    test('it should have last reading of: £29.04 - 27/10/2019 at 11:20', async () => {
+
+        db.readings.add({
+            date: new Date(Date.parse('2019/10/26 19:41')).toISOString(),
+            reading: '34.19'
+        });
+
+        db.readings.add({
+            date: new Date(Date.parse('2019/10/27 10:21')).toISOString(),
+            reading: '29.23'
+        });
+
+        db.readings.add({
+            date: new Date(Date.parse('2019/10/27 11:20')).toISOString(),
+            reading: '29.04'
+        });
+       
+        
+        const {findByTestId} = render(<App dataStore={db}/>);
+        
+        expect(await findByTestId('last-reading')).toBeInTheDocument();
+        expect(await findByTestId('last-reading-price')).toBeInTheDocument();
+        expect(await findByTestId('last-reading-price')).toHaveTextContent('£29.04');
+
+        expect(await findByTestId('last-reading-date')).toBeInTheDocument();
+        expect(await findByTestId('last-reading-date')).toHaveTextContent('27/10/2019 at 11:20');
+    });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -12,58 +12,45 @@ type AppProps = {
 
 const App: FunctionComponent<AppProps> = ({dataStore}) => {
     // App state: last reading is the last known input, previous readings is the DB readings
-    const [latestReading, updateLatestReading] = useState<IReading | undefined>(undefined);
     const [previousReadings, updatePreviousReadings] = useState<DBReading[] | undefined>(undefined);
-
-
-    // Have an effect hook to update our previous readings only if our DB readings changes
-    useEffect(() => {
-        const noPreviousReadings = (!previousReadings || previousReadings.length === 0); 
-        const readingDatesMismatch = (latestReading && previousReadings && previousReadings.length > 0 && (previousReadings[0]).date !== latestReading.date);
-
-        if (!latestReading || noPreviousReadings || readingDatesMismatch) {
-            dataStore.readings.toArray().then((res) => {
-                res.reverse()
-                updatePreviousReadings(res);
-            });
-        }
-
-    }, [dataStore.readings, latestReading, previousReadings]);
-
-
-    // Have an effectHook to update our previous reading if loading for first time
-    useEffect(() => {
-        if (!latestReading && previousReadings) {
-            updateLatestReading(previousReadings[0]);
-        }
-    }, [previousReadings, latestReading]);
-
   
+    const addNewReading = function(reading: string){
+        saveReading(reading);
+        refreshStateFromDB();
+    };
+
     const saveReading = function(reading: string){
         const readingObj: IReading = {
             date: new Date().toISOString(),
             reading: reading
         }
 
-        //Update our state
-        updateLatestReading(readingObj);
-
-        //Save to db
         dataStore.readings.add(readingObj);
-
     };
 
+    const refreshStateFromDB = async function(){        
+        const DBRes = await dataStore.readings.toArray();
+
+        if(DBRes.length > 0){
+            updatePreviousReadings(DBRes.reverse());
+        }
+    };
+
+    // Ensure state and db are sync'd on first run
+    useEffect(() => {
+        refreshStateFromDB();        
+    }, []);
 
     return (
         <div className="App">
-            {latestReading && parseFloat(latestReading.reading as string) < 10 && <div id="alert-banner">Running low, go top up!!!</div>}
+            {previousReadings && previousReadings[0] && parseFloat(previousReadings[0].reading as string) < 10 && <div id="alert-banner">Running low, go top up!!!</div>}
             <header className="App-header">
                 <h1>Leccie Monitor</h1>
                 <p>Don&rsquo;t be left in the dark&hellip;</p>
-                {latestReading && <p>Last reading: <strong className={parseFloat(latestReading.reading as string) < 10 ? 'danger' : ''}>&pound;{latestReading.reading}</strong> - <span className="date">{formatRelativeToDate(latestReading.date as string, new Date().toISOString())}</span></p>}
-                <ReadingForm onSuccess={saveReading} />
+                {previousReadings && previousReadings[0] && <p data-testid="last-reading">Last reading: <strong data-testid="last-reading-price"  className={parseFloat(previousReadings[0].reading as string) < 10 ? 'danger' : ''}>&pound;{previousReadings[0].reading}</strong> - <span data-testid="last-reading-date" className="date">{formatRelativeToDate(previousReadings[0].date as string, new Date().toISOString())}</span></p>}
+                <ReadingForm onSuccess={addNewReading} />
             </header>
-            <ReadingTable previousReadings={previousReadings} lastReading={latestReading}/>
+            <ReadingTable previousReadings={previousReadings}/>
         </div>
     );
 }
