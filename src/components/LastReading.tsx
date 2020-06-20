f00549fbe64bfd06f50755b1eccbd086a2ef7342
Huntly Cameron <huntly.cameron@gmail.com>
2020-06-20 10:57:40 +0100

    Fix space in html introducted by previous refactor

--- src/components/LastReading.tsx
+++ src/components/LastReading.tsx
@@ -14,8 +14,8 @@ const LastReading: FunctionComponent<LastReadingProps> = ({reading, isRunningLow
     const readingDate = formatRelativeToDate(reading.date as string, new Date().toISOString());
 
     return (
-        <p data-testid="last-reading">Last reading:
-            <strong data-testid="last-reading-price"  className={isRunningLow ? 'danger' : ''}>&pound;{reading.reading}</strong> -
+        <p data-testid="last-reading">Last reading:&nbsp;
+            <strong data-testid="last-reading-price"  className={isRunningLow ? 'danger' : ''}>&pound;{reading.reading}</strong> -&nbsp;
             <span data-testid="last-reading-date" className="date">{readingDate}</span>
         </p>
     );
