2d578dcb1342e58a61d593ab723d6a76491ab435
Huntly Cameron <huntly.cameron@gmail.com>
2020-06-28 16:46:11 +0100

    remove unused imports in input form test

--- src/components/InputForm.test.tsx
+++ src/components/InputForm.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReadingForm from './InputForm';
-import { render, screen, fireEvent, waitFor, cleanup, RenderResult } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 
 
